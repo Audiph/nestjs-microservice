98c05dbc051904bcb57120e16f0656bc8b4bfafe
audiph <lagarde.marcjoseph@gmail.com>
2024-04-07 17:19:32 +0800

    add mongodb and complete orders module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
 import { OrdersRepository } from './orders.repository';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -13,7 +13,7 @@ import { Order, OrderSchema } from './schemas/order.schema';
     ConfigModule.forRoot({
       isGlobal: true,
       validationSchema: Joi.object({
-        MONGODB_URL: Joi.string().required(),
+        MONGODB_URI: Joi.string().required(),
       }),
       envFilePath: './apps/orders/.env',
     }),
